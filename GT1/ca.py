bdda130ac0d9a0a641d287fadae4556e57007b03
JRB-Prod-UVA <117186330+JRB-Prod-UVA@users.noreply.github.com>
2024-02-28 23:28:00 +0100

    Implementatie bijna af (nog ongetest)

--- GT1/ca.py
+++ GT1/ca.py
@@ -7,6 +7,10 @@ from itertools import islice
 from pyics import Model
 
 
+# Random seed constant, for verifiable results
+RNG_SEED = -89234380
+
+
 # payoff matrix for Prisoner's Dilemma
 rewards = {'CC': (3, 3), 'CD': (0, 5), 'DC': (5, 0), 'DD': (1, 1)}
 
@@ -31,35 +35,42 @@ class Population(CAsim):
     def __init__(self,
                  seq_len=71,
                  hist_len=6,
-                 rounds=50,
-                 generations=None,
-                 population_size=None,
-                 crossover_prob=None,
-                 mutation_prob=None):
+                 rounds=1000,
+                 generations=1000,
+                 population_size=1000,
+                 crossover_prob=0.95,
+                 mutation_prob=0.001):
 
         CASim.__init__(self)
         self.__initialize_prob_params__(crossover_prob, mutation_prob)
-        self.__initialize_chrom_params__(seq_len, hist_len, rounds,
-                                         population_size)
+        self.__initialize_match_params__(rounds, generations, population_size)
+        self.__initialize_chrom_params__(seq_len, hist_len)
+
+    def __initialize_match_params__(self, rounds, generations,
+                                    population_size):
+        self.rounds = rounds
+        self.gens = generations
+        self.pop_size = population_size
 
     def __initialize_prob_params__(self, crossover_prob, mutation_prob):
         self.crossover_prob = crossover_prob
         self.mutation_prob = mutation_prob
+        self.rng = np.random.default_rng(RNG_SEED)
 
-    def __initialize_chrom_params__(self, seq_len, hist_len, rounds,
-                                    population_size):
+    def __initialize_chrom_params__(self, seq_len, hist_len):
         self.seq_len = seq_len
         self.hist_len = hist_len
-        self.rounds = rounds
-        self.pop_size = population_size
         self.pop_hist = [deque(maxlen=self.hist_len)
                          for _ in range(self.pop_size)]
         self.chrom = np.random.choice(
             ['C', 'D'], size=(self.pop_size, self.seq_len))
 
+    def __true_by_chance(self, probability):
+        return self.rng.uniform(0, 1) <= probability
+
     def get_fitness(self, points, c=2):
         fitness = np.zeros(self.pop_size)
-        
+
         fitness_average = np.mean(fitness)
         fitness_max = np.max(fitness)
 
@@ -68,52 +79,66 @@ class Population(CAsim):
 
         def calculate_b(c, average, maximum):
             return average * (average - (c * average)) / (maximum - average)
-        
-        # calculate parameters a and b described by Goldberg
+
+        # calculate parameters a and b described by Goldberg.
         a = calculate_a(c, fitness_average, fitness_max)
         b = calculate_b(c, fitness_average, fitness_max)
-        
+
         for i in range(self.pop_size):
-            # scale raw fitness scores
+            # scale raw fitness scores.
             fitness[i] = a * points[i] + b
         return fitness
 
     def extend_population(self, fitness):
+        # helper function to mutate a given chromosome.
+        def mutate_chromosome(chrom_str: str) -> str:
+            """This function will mutate 'chrom_str' by copying the letters and
+            by chance, invert some letters. A mutated string copy is
+            returned."""
+            mutated_copy = []
+            for letter in chrom_str:
+                # maybe mutate chromosome.
+                if self.__true_by_chance(self.mutation_prob):
+                    mutated_copy.append('D' if letter == 'C' else 'C')
+                else:
+                    mutated_copy.append(letter)
+
+            return ''.join(mutated_copy)
+
+        fitness_prob = fitness / np.sum(fitness)
         new_pop = []
         parent_1, parent_2 = '', ''
         offspring_1, offspring_2 = '', ''
         mutate_1, mutate_2 = '', ''
-    
-        fitness_prob = fitness / np.sum(fitness)
-        parents_idx = np.random.choice(np.arange(self.pop_size), size=2, p=fitness_prob)
-        parent_1, parent_2 = self.pop[parents_idx]
 
-        # TO-DO: need to create a full new population
+        for _ in range(int(np.ceil(self.pop_size / 2))):
+            parents_idx = np.random.choice(
+                self.pop_size, size=2, p=fitness_prob, replace=False)
+            parent_1, parent_2 = self.pop[parents_idx]
 
-        # determine if crossover should take place
-        if random.uniform(0, 1) < self.crossover_prob:
-            crossover_pos = random.randrange(67)
-            offspring_1 = parent_1[:crossover_pos] + parent_2[crossover_pos:]
-            offspring_2 = parant_2[:crossover_pos] + parent_1[crossover_pos:]
-        else:
-            offspring_1 = parent_1
-            offspring_2 = parent_2
-        
-        for a, b in zip(offspring_1, offspring_2):
-            # maybe mutate chromosome
-            if random.uniformm(0, 1) < mutation_prob:
-                if a == 'C':
-                    mutate_1 += 'D'
-                else:
-                    mutate_1 += 'C'
-            if random.uniform(0, 1) < mutation_prob:
-                if a == 'C':
-                    mutate_2 += 'D'
-                else:
-                    mutate_2 += 'C'
-        new_pop.append(mutate_1 mutate_2)
-        # Note: if n is odd, one new population member can be described at random.
-        return new_pop
+            # determine if crossover should take place.
+            if self.__true_by_chance(self.crossover_prob):
+                crossover_pos = self.rng.integers(1, self.seq_len - 1,
+                                                  endpoint=False)
+                offspring_1 = parent_1[:crossover_pos] + \
+                    parent_2[crossover_pos:]
+                offspring_2 = parent_2[:crossover_pos] + \
+                    parent_1[crossover_pos:]
+            else:
+                offspring_1 = parent_1
+                offspring_2 = parent_2
+
+            mutate_1 = mutate_chromosome(offspring_1)
+            mutate_2 = mutate_chromosome(offspring_2)
+            new_pop += [mutate_1, mutate_2]
+        # Note: if n is odd, one new population member can be described at
+        # random.
+        if len(new_pop) > self.pop_size:
+            del_idx = self.rng.integers(-2, -1, endpoint=True)
+            new_pop.pop(del_idx)
+        assert len(new_pop) == self.pop_size
+
+        return np.array(new_pop)
 
     def run_prisoners_dilemma(self, num_runs, num_generations):
         for i in range(num_runs):
@@ -151,11 +176,11 @@ class Population(CAsim):
             elif n < 3:
                 # If we have less then 3 rounds of history we only look at the
                 # moves of opponent to make our decision (max 2).
-               mem_a = ''.join(islice(self.pop_hist[j], 0, None, 2))
-               mem_b = ''.join(islice(self.pop_hist[i], 0, None, 2))
-               assert 0 < len(mem_a) <= 2 and 0 < len(mem_b) <= 2
-               mem_idx_a = hist_to_idx[mem_a]
-               mem_idx_b = hist_to_idx[mem_b]
+                mem_a = ''.join(islice(self.pop_hist[j], 0, None, 2))
+                mem_b = ''.join(islice(self.pop_hist[i], 0, None, 2))
+                assert 0 < len(mem_a) <= 2 and 0 < len(mem_b) <= 2
+                mem_idx_a = hist_to_idx[mem_a]
+                mem_idx_b = hist_to_idx[mem_b]
             else:
                 # We have at least three rounds, we only have to convert the
                 # queue to a string.
